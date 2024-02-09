4b07f7ce865805d7dcb17ee74859ef67bb88daba
TomiHenriksson8 <tomihen01@gmail.com>
2024-02-09 18:36:35 +0200

    s

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,5 +1,7 @@
 import {Link, Outlet} from "react-router-dom";
 import { useUserContext } from "../hooks/ContextHooks";
+import SearchBar from "../components/SearchBar";
+import SearchResultsList from "../components/SearchResultList";
 
 
 const Layout = () => {
@@ -13,24 +15,37 @@ const Layout = () => {
   return (
     <>
       <header>
-        <h1>My app</h1>
-        <nav>
+        <nav className="navbar">
           <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
+            <div className="left-group">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+            </div>
+
+            {/* <div className="search-bar-container">
+              <SearchBar  setResults={setResults}/>
+              <SearchResultsList results={results}/>
+            </div> */}
+
             {user && (
               // Show these links only if the user is logged in
               <>
-                <li>
-                  <Link to="/profile">Profile</Link>
-                </li>
-                <li>
-                  <Link to="/upload">Upload</Link>
-                </li>
-                <li>
-                  <Link to="/logout">Logout</Link>
-                </li>
+                <div className="right-group">
+                  <li>
+                    <Link to="/upload">
+                      <button className="upload-btn">Upload</button>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/profile">
+                      <img className="profile-icon" src="https://place-hold.it/170x170.jpg&text=Pic2&fontsize=0" alt="Profile" />
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/logout">Logout</Link>
+                  </li>
+                </div>
               </>
             )}
             {!user && (
